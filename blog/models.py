4bb06d9b7ba20fb41f946421c1c133bf6fd9ea79
нияз <niyaz@MacBook-Air-niaz.local>
2022-10-18 21:29:02 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -19,6 +16,3 @@ class Post(models.Model):
     def __str__(self):
          return self.title
         
-class Exemple():
-    poss
-    
